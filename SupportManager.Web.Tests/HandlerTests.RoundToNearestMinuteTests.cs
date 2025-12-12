229df09cfbb7b62108e926b47e07b2e5569e7ee4
Amer Alhasoun <a.alhasoun@viscon.eu>
2025-12-12 10:38:26 +0100

    add HandlerTests.RoundToNearestMinuteTests.cs

---
+++ SupportManager.Web.Tests/HandlerTests.RoundToNearestMinuteTests.cs
@@ -0,0 +1,53 @@
+using Shouldly;
+using Xunit;
+using static SupportManager.Web.Areas.Teams.Pages.Report.DailyModel;
+
+namespace SupportManager.Web.Tests
+{
+    public partial class HandlerTests
+    {
+        public class RoundToNearestMinuteTests
+        {
+            [Fact]
+            public void Rounds_down_when_less_than_30_seconds()
+            {
+                var input = TimeSpan.FromSeconds(29);
+
+                var result = Handler.RoundToNearestMinute(input);
+
+                result.ShouldBe(TimeSpan.Zero);
+            }
+
+            [Fact]
+            public void Rounds_up_at_30_seconds()
+            {
+                var input = TimeSpan.FromSeconds(30);
+
+                var result = Handler.RoundToNearestMinute(input);
+
+                result.ShouldBe(TimeSpan.FromMinutes(1));
+            }
+
+            [Fact]
+            public void Rounds_up_when_more_than_30_seconds()
+            {
+                var input = TimeSpan.FromSeconds(91); // 1:31
+
+                var result = Handler.RoundToNearestMinute(input);
+
+                result.ShouldBe(TimeSpan.FromMinutes(2));
+            }
+
+            [Fact]
+            public void Exact_minutes_are_not_changed()
+            {
+                var input = TimeSpan.FromMinutes(5);
+
+                var result = Handler.RoundToNearestMinute(input);
+
+                result.ShouldBe(TimeSpan.FromMinutes(5));
+            }
+        }
+        
+    }
+}
