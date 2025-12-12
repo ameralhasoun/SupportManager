26dddd1203277aed46b7a7b1cef6bfc0d328eca4
Amer Alhasoun <a.alhasoun@viscon.eu>
2025-12-12 12:12:25 +0100

    add // Arrange, // Act and // Assert for HandlerTests.RoundTimestampToNearestMinute.cs

--- SupportManager.Web.Tests/HandlerTests.RoundTimestampToNearestMinute.cs
+++ SupportManager.Web.Tests/HandlerTests.RoundTimestampToNearestMinute.cs
@@ -11,32 +11,42 @@ namespace SupportManager.Web.Tests
             [Fact]
             public void Rounds_down_when_seconds_less_than_30()
             {
+                // Arrange
                 var input = new DateTimeOffset(2025, 1, 1, 10, 15, 29, TimeSpan.FromHours(1));
 
+                // Act
                 var result = Handler.RoundTimestampToNearestMinute(input);
 
+                // Assert
                 result.ShouldBe(new DateTimeOffset(2025, 1, 1, 10, 15, 0, TimeSpan.FromHours(1)));
             }
 
             [Fact]
             public void Rounds_up_when_seconds_are_30_or_more()
             {
+                // Arrange
                 var input = new DateTimeOffset(2025, 1, 1, 10, 15, 30, TimeSpan.FromHours(1));
 
+                // Act
                 var result = Handler.RoundTimestampToNearestMinute(input);
 
+                // Assert
                 result.ShouldBe(new DateTimeOffset(2025, 1, 1, 10, 16, 0, TimeSpan.FromHours(1)));
             }
 
             [Fact]
             public void Rounds_up_across_hour_boundary()
             {
+                // Arrange
                 var input = new DateTimeOffset(2025, 1, 1, 10, 59, 45, TimeSpan.Zero);
 
+                // Act
                 var result = Handler.RoundTimestampToNearestMinute(input);
 
+                // Assert
                 result.ShouldBe(new DateTimeOffset(2025, 1, 1, 11, 0, 0, TimeSpan.Zero));
             }
         }
     }
 }
+
